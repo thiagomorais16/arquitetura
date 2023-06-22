50c281eaced022406d1e9198886d9914222869c1
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-06-21 22:50:43 -0300

    update script

--- script.js
+++
@@ -1,8 +0,0 @@
-function handleMediaQuery(mediaQuery) {
-    const h1 = document.querySelector("h1");
-    const h2 = document.querySelector("h2");
-    const nav = document.querySelector("nav");
-    const ul = document.querySelector("nav ul");
-    const li = document.querySelectorAll("nav li");
-
-    
